6813ad63e285a9efc4bd63d77216c60653217a5e
sbstnlambert <sbstn.lambert@gmail.com>
2022-05-04 14:45:12 +0200

    created invoice service methods

--- invoice/src/main/java/be/technifutur/invoice/InvoiceService.java
+++ invoice/src/main/java/be/technifutur/invoice/InvoiceService.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-//public class InvoiceService {
-//
-//    private List<Invoice> invoices = new ArrayList<>();
-//
-//    public void createInvoice(UUID bookingRef, int numberOfNightsBooked) {
-//
-//    }
-//
-//    public List<Invoice> getInvoices() {
-//        return null;
-//    }
-//}
+public class InvoiceService {
+
+    private List<Invoice> invoices = new ArrayList<>();
+
+    public void createInvoice(UUID bookingRef, int numberOfNightsBooked) {
+        this.invoices.add(new Invoice(bookingRef, numberOfNightsBooked));
+    }
+
+    public List<Invoice> getInvoices() {
+        return this.invoices;
+    }
+}
